7a39d74f76c583a54e635185b1239e733c58760a
Karem <karem.abo@campus.technion.ac.il>
2025-06-05 10:53:35 +0300

    needs testing

--- cacheSim.cpp
+++ cacheSim.cpp
@@ -1,7 +1,11 @@
+/* 046267 Computer Architecture - Winter 20/21 - HW #2 */
+
 #include <cstdlib>
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include "classesForCache.cpp"
+
 
 using std::FILE;
 using std::string;
@@ -12,88 +16,107 @@ using std::ifstream;
 using std::stringstream;
 
 int main(int argc, char **argv) {
+    if (argc < 19) {
+        cerr << "Not enough arguments" << endl;
+        return 0;
+    }
 
-	if (argc < 19) {
-		cerr << "Not enough arguments" << endl;
-		return 0;
-	}
+    // Get input arguments
 
-	// Get input arguments
+    // File
+    // Assuming it is the first argument
+    char* fileString = argv[1];
+    ifstream file(fileString); //input file stream
+    string line;
+    if (!file || !file.good()) {
+        // File doesn't exist or some other error
+        cerr << "File not found" << endl;
+        return 0;
+    }
+    ///example 1 :
+    //unsigned MemCyc = 100, BSize = 3, L1Size = 4, L2Size = 6, L1Assoc = 1,L2Assoc = 0, L1Cyc = 1, L2Cyc = 5, WrAlloc = 1;
 
-	// File
-	// Assuming it is the first argument
-	char* fileString = argv[1];
-	ifstream file(fileString); //input file stream
-	string line;
-	if (!file || !file.good()) {
-		// File doesn't exist or some other error
-		cerr << "File not found" << endl;
-		return 0;
-	}
 
-	unsigned MemCyc = 0, BSize = 0, L1Size = 0, L2Size = 0, L1Assoc = 0,
-			L2Assoc = 0, L1Cyc = 0, L2Cyc = 0, WrAlloc = 0;
+    ///example 2 :
+    // unsigned MemCyc = 50, BSize = 4, L1Size = 6, L2Size = 8, L1Assoc = 1,L2Assoc = 2, L1Cyc = 2, L2Cyc = 4, WrAlloc = 1;
 
-	for (int i = 2; i < 19; i += 2) {
-		string s(argv[i]);
-		if (s == "--mem-cyc") {
-			MemCyc = atoi(argv[i + 1]);
-		} else if (s == "--bsize") {
-			BSize = atoi(argv[i + 1]);
-		} else if (s == "--l1-size") {
-			L1Size = atoi(argv[i + 1]);
-		} else if (s == "--l2-size") {
-			L2Size = atoi(argv[i + 1]);
-		} else if (s == "--l1-cyc") {
-			L1Cyc = atoi(argv[i + 1]);
-		} else if (s == "--l2-cyc") {
-			L2Cyc = atoi(argv[i + 1]);
-		} else if (s == "--l1-assoc") {
-			L1Assoc = atoi(argv[i + 1]);
-		} else if (s == "--l2-assoc") {
-			L2Assoc = atoi(argv[i + 1]);
-		} else if (s == "--wr-alloc") {
-			WrAlloc = atoi(argv[i + 1]);
-		} else {
-			cerr << "Error in arguments" << endl;
-			return 0;
-		}
-	}
 
-	while (getline(file, line)) {
+    ///example 3 :
+    //   unsigned MemCyc = 10, BSize = 2, L1Size = 4, L2Size = 4, L1Assoc = 1,  L2Assoc = 2, L1Cyc = 1, L2Cyc = 5, WrAlloc = 1;
 
-		stringstream ss(line);
-		string address;
-		char operation = 0; // read (R) or write (W)
-		if (!(ss >> operation >> address)) {
-			// Operation appears in an Invalid format
-			cout << "Command Format error" << endl;
-			return 0;
-		}
 
-		// DEBUG - remove this line
-		cout << "operation: " << operation;
+    unsigned MemCyc = 0, BSize = 0, L1Size = 0, L2Size = 0, L1Assoc = 0,
+            L2Assoc = 0, L1Cyc = 0, L2Cyc = 0, WrAlloc = 0;
 
-		string cutAddress = address.substr(2); // Removing the "0x" part of the address
+    for (int i = 2; i < 19; i += 2) {
+        string s(argv[i]);
+        if (s == "--mem-cyc") {
+            MemCyc = atoi(argv[i + 1]);
+        } else if (s == "--bsize") {
+            BSize = atoi(argv[i + 1]);
+        } else if (s == "--l1-size") {
+            L1Size = atoi(argv[i + 1]);
+        } else if (s == "--l2-size") {
+            L2Size = atoi(argv[i + 1]);
+        } else if (s == "--l1-cyc") {
+            L1Cyc = atoi(argv[i + 1]);
+        } else if (s == "--l2-cyc") {
+            L2Cyc = atoi(argv[i + 1]);
+        } else if (s == "--l1-assoc") {
+            L1Assoc = atoi(argv[i + 1]);
+        } else if (s == "--l2-assoc") {
+            L2Assoc = atoi(argv[i + 1]);
+        } else if (s == "--wr-alloc") {
+            WrAlloc = atoi(argv[i + 1]);
+        } else {
+            cerr << "Error in arguments" << endl;
+            return 0;
+        }
+    }
 
-		// DEBUG - remove this line
-		cout << ", address (hex)" << cutAddress;
 
-		unsigned long int num = 0;
-		num = strtoul(cutAddress.c_str(), NULL, 16);
+    /// here we have to initialize our cache/memory and then work with it
+    CacheManager *main_controller = new CacheManager(
+            BSize, WrAlloc, MemCyc,
+            L1Size, L1Assoc, L1Cyc,
+            L2Size, L2Assoc, L2Cyc
+    );
 
-		// DEBUG - remove this line
-		cout << " (dec) " << num << endl;
+    while (getline(file, line)) {
 
-	}
+        stringstream ss(line);
+        string address;
+        char operation = 0; // read (R) or write (W)
+        if (!(ss >> operation >> address)) {
+            // Operation appears in an Invalid format
+            cout << "Command Format error" << endl;
+            return 0;
+        }
 
-	double L1MissRate;
-	double L2MissRate;
-	double avgAccTime;
+        // DEBUG - remove this line
+        //cout << "operation: " << operation;
 
-	printf("L1miss=%.03f ", L1MissRate);
-	printf("L2miss=%.03f ", L2MissRate);
-	printf("AccTimeAvg=%.03f\n", avgAccTime);
+        string cutAddress = address.substr(2); // Removing the "0x" part of the address
 
-	return 0;
+        // DEBUG - remove this line
+        //cout << ", address (hex)" << cutAddress;
+
+        unsigned long int num = 0;
+        num = strtoul(cutAddress.c_str(), NULL, 16);
+
+        // DEBUG - remove this line
+        //cout << " (dec) " << num << endl;
+
+        main_controller->access(static_cast<uint32_t>(num), operation);
+    }
+
+    double L1MissRate;
+    double L2MissRate;
+    double avgAccTime;
+    main_controller->finalizeStats(L1MissRate, L2MissRate, avgAccTime);
+    printf("L1miss=%.03f ", L1MissRate);
+    printf("L2miss=%.03f ", L2MissRate);
+    printf("AccTimeAvg=%.03f\n", avgAccTime);
+
+    return 0;
 }
--- classesForCache.cpp
+++ classesForCache.cpp
@@ -1,24 +1,327 @@
-//
-// Created by karim on 5/25/2025.
-//
-#include <iostream>
-#include <vector>
-#include <math.h>
-using std::vector;
+#include <cmath>
+#include <cstdint>
+#include <cstdlib>
 
-class Block{
-public:
-    bool isValid;
-    bool isDirty;
-    long tag;
-    unsigned long address;
-    int counter;
 
-    Block(long tag, unsigned long pc, bool state) : isValid(true), isDirty(state), tag(tag),
-    address(pc), counter(0){}
-    Block() : isValid(false), isDirty(false){}
+static unsigned extractSetIndex(uint32_t addr, unsigned blkBits, unsigned numSets) {
+    if (numSets <= 1) {
+        return 0;
+    }
+    unsigned setBits = static_cast<unsigned>(std::log2(numSets));
+    return (addr >> blkBits) & ((1u << setBits) - 1);
+}
+
+static uint32_t extractTag(uint32_t addr, unsigned blkBits, unsigned numSets) {
+    unsigned setBits = (numSets <= 1 ? 0u : static_cast<unsigned>(std::log2(numSets)));
+    unsigned shiftAmt = blkBits + setBits;
+    if (shiftAmt >= 32u) {
+        return 0u;
+    }
+    return (addr >> shiftAmt);
+}
+
+static uint32_t rebuildBlockAddress(uint32_t tag, unsigned setIdx, unsigned blkBits, unsigned numSets) {
+    unsigned setBits = (numSets <= 1 ? 0u : static_cast<unsigned>(std::log2(numSets)));
+    return (tag << (setBits + blkBits)) | (setIdx << blkBits);
+}
+
+enum AllocPolicy {
+    ALLOC_WRITE_ALLOC   = 1,
+    ALLOC_NO_WRITE_ALLOC = 0
 };
 
-class Cache{};
+class CacheLine {
+public:
+    bool    valid;
+    bool    dirty;
+    uint32_t tag;
+    int     lruRank;
 
-class CacheSimulator{};
+    CacheLine()
+            : valid(false), dirty(false), tag(static_cast<uint32_t>(-1)), lruRank(0)
+    {}
+
+    void reset() {
+        valid   = false;
+        dirty   = false;
+        tag     = static_cast<uint32_t>(-1);
+        lruRank = 0;
+    }
+};
+
+
+class LevelCache {
+public:
+    unsigned     blkBits;
+    unsigned     sizeBits;
+    unsigned     cycTicks;
+    AllocPolicy  policy;
+    unsigned     numWays;
+    unsigned     totalBlocks;
+    unsigned     numSets;
+
+    CacheLine**  lines;
+    unsigned     accessCnt;
+    unsigned     missCnt;
+
+    LevelCache(unsigned blkBits_,
+               unsigned sizeBits_,
+               unsigned cycTicks_,
+               unsigned policyFlag,
+               unsigned assocBits_)
+            : blkBits(blkBits_),
+              sizeBits(sizeBits_),
+              cycTicks(cycTicks_),
+              policy(static_cast<AllocPolicy>(policyFlag)),
+              numWays(static_cast<unsigned>(std::pow(2, assocBits_))),
+              accessCnt(0),
+              missCnt(0)
+    {
+        totalBlocks = static_cast<unsigned>(std::pow(2, (sizeBits - blkBits)));
+        numSets     = totalBlocks / numWays;
+        lines = new CacheLine*[numSets];
+        for (unsigned s = 0; s < numSets; ++s) {
+            lines[s] = new CacheLine[numWays];
+            for (unsigned w = 0; w < numWays; ++w) {
+                lines[s][w].reset();
+            }
+        }
+    }
+
+    ~LevelCache() {
+        for (unsigned s = 0; s < numSets; ++s) {
+            delete[] lines[s];
+        }
+        delete[] lines;
+    }
+
+    void promoteLRU(unsigned setIdx, unsigned wayIdx) {
+        int oldRank = lines[setIdx][wayIdx].lruRank;
+        lines[setIdx][wayIdx].lruRank = static_cast<int>(numWays - 1);
+        for (unsigned w = 0; w < numWays; ++w) {
+            if (w != wayIdx && lines[setIdx][w].lruRank > oldRank) {
+                lines[setIdx][w].lruRank--;
+            }
+        }
+    }
+
+    unsigned pickEvictWay(unsigned setIdx) const {
+        for (unsigned w = 0; w < numWays; ++w) {
+            if (lines[setIdx][w].lruRank == 0) {
+                return w;
+            }
+        }
+        return 0;
+    }
+
+    bool readAccess(uint32_t addr) {
+        accessCnt++;
+        unsigned setIdx = extractSetIndex(addr, blkBits, numSets);
+        uint32_t tg     = extractTag(addr, blkBits, numSets);
+
+        for (unsigned w = 0; w < numWays; ++w) {
+            CacheLine& ln = lines[setIdx][w];
+            if (ln.valid && ln.tag == tg) {
+                promoteLRU(setIdx, w);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    bool writeAccess(uint32_t addr, bool isEvictRef) {
+        if (!isEvictRef) {
+            accessCnt++;
+        }
+        unsigned setIdx = extractSetIndex(addr, blkBits, numSets);
+        uint32_t tg     = extractTag(addr, blkBits, numSets);
+
+        for (unsigned w = 0; w < numWays; ++w) {
+            CacheLine& ln = lines[setIdx][w];
+            if (ln.valid && ln.tag == tg) {
+                ln.dirty = true;
+                promoteLRU(setIdx, w);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    void loadBlock(uint32_t addr,
+                   int32_t* evictedOut,
+                   bool isSecondLevel,
+                   LevelCache* lowerLevel,
+                   char opType,
+                   bool fillFromLower)
+    {
+        unsigned setIdx = extractSetIndex(addr, blkBits, numSets);
+        uint32_t tg     = extractTag(addr, blkBits, numSets);
+
+        // 1) If any invalid line, fill it
+        for (unsigned w = 0; w < numWays; ++w) {
+            CacheLine& ln = lines[setIdx][w];
+            if (!ln.valid) {
+                ln.valid = true;
+                ln.tag   = tg;
+                ln.dirty = (opType == 'w' && !fillFromLower);
+                promoteLRU(setIdx, w);
+                *evictedOut = -1;
+                return;
+            }
+        }
+
+        // 2) All lines valid → evict LRU
+        unsigned victimWay = pickEvictWay(setIdx);
+        CacheLine& victim = lines[setIdx][victimWay];
+
+        bool wasDirty   = victim.dirty;
+        uint32_t oldTag = victim.tag;
+        uint32_t evAddr = rebuildBlockAddress(
+                oldTag,
+                setIdx,
+                blkBits,
+                numSets
+        );
+        // Install new block
+        victim.tag   = tg;
+        victim.valid = true;
+        victim.dirty = (opType == 'w' && !fillFromLower);
+        promoteLRU(setIdx, victimWay);
+
+        *evictedOut = static_cast<int32_t>(evAddr);
+        // If we evicted a dirty line from L1, write it back to L2
+        if (wasDirty && !isSecondLevel) {
+            lowerLevel->writeAccess(evAddr, true);
+        }
+    }
+
+
+    void invalidate(uint32_t addr) {
+        unsigned setIdx = extractSetIndex(addr, blkBits, numSets);
+        uint32_t tg     = extractTag(addr, blkBits, numSets);
+        for (unsigned w = 0; w < numWays; ++w) {
+            CacheLine& ln = lines[setIdx][w];
+            if (ln.valid && ln.tag == tg) {
+                ln.valid   = false;
+                ln.dirty   = false;
+                ln.tag     = static_cast<uint32_t>(-1);
+                ln.lruRank = 0;
+                return;
+            }
+        }
+    }
+};
+
+
+class CacheManager {
+public:
+    LevelCache firstLevel;
+    LevelCache secondLevel;
+    unsigned    memCycles;
+    double      totalCycles;
+
+    CacheManager(unsigned blkBits,
+                 unsigned wrAlloc,
+                 unsigned memCyc,
+                 unsigned l1SizeBits,
+                 unsigned l1AssocBits,
+                 unsigned l1Cyc,
+                 unsigned l2SizeBits,
+                 unsigned l2AssocBits,
+                 unsigned l2Cyc)
+            : firstLevel(blkBits, l1SizeBits, l1Cyc, wrAlloc, l1AssocBits),
+              secondLevel(blkBits, l2SizeBits, l2Cyc, wrAlloc, l2AssocBits),
+              memCycles(memCyc),
+              totalCycles(0.0)
+    {}
+
+    ~CacheManager() = default;
+
+
+    void access(uint32_t addr, char op) {
+        int32_t evictedAddr = -1;
+
+        if (op == 'r') {
+            // 1) Try L1 read
+            bool hitL1 = firstLevel.readAccess(addr);
+            totalCycles += static_cast<double>(firstLevel.cycTicks);
+            if (hitL1) return;
+
+            // L1 miss
+            firstLevel.missCnt++;
+            // 2) Try L2 read
+            bool hitL2 = secondLevel.readAccess(addr);
+            totalCycles += static_cast<double>(secondLevel.cycTicks);
+            if (!hitL2) {
+                // L2 miss → main memory
+                secondLevel.missCnt++;
+                totalCycles += static_cast<double>(memCycles);
+                // Load into L2
+                secondLevel.loadBlock(addr, &evictedAddr, true,
+                                      &secondLevel, 'r', false);
+                if (evictedAddr != -1) {
+                    firstLevel.invalidate(static_cast<uint32_t>(evictedAddr));
+                }
+                // Load into L1
+                firstLevel.loadBlock(addr, &evictedAddr, false,
+                                     &secondLevel, 'r', true);
+            } else {
+                // L2 hit → fetch into L1
+                firstLevel.loadBlock(addr, &evictedAddr, false,
+                                     &secondLevel, 'r', true);
+            }
+        }
+        else if (op == 'w') {
+            // 1) Try L1 write
+            bool hitL1 = firstLevel.writeAccess(addr, false);
+            totalCycles += static_cast<double>(firstLevel.cycTicks);
+            if (hitL1) return;
+
+            // L1 miss
+            firstLevel.missCnt++;
+            // 2) Try L2 write
+            bool hitL2 = secondLevel.writeAccess(addr, false);
+            totalCycles += static_cast<double>(secondLevel.cycTicks);
+            if (hitL2) {
+                // On L2 hit, if Write‐Allocate, fetch into L1
+                if (secondLevel.policy == ALLOC_WRITE_ALLOC) {
+                    firstLevel.loadBlock(addr, &evictedAddr, false,
+                                         &secondLevel, 'w', true);
+                }
+                return;
+            }
+
+            // L2 miss → main memory
+            secondLevel.missCnt++;
+            totalCycles += static_cast<double>(memCycles);
+            if (secondLevel.policy == ALLOC_WRITE_ALLOC) {
+                // Allocate in L2, then allocate in L1
+                secondLevel.loadBlock(addr, &evictedAddr, true,
+                                      &secondLevel, 'w', false);
+                if (evictedAddr != -1) {
+                    firstLevel.invalidate(static_cast<uint32_t>(evictedAddr));
+                }
+                firstLevel.loadBlock(addr, &evictedAddr, false,
+                                     &secondLevel, 'w', true);
+            }
+
+        }
+    }
+
+    void finalizeStats(double& l1MissRate, double& l2MissRate, double& avgTime) const {
+        if (firstLevel.accessCnt > 0) {
+            l1MissRate = static_cast<double>(firstLevel.missCnt)
+                         / static_cast<double>(firstLevel.accessCnt);
+            avgTime    = totalCycles / static_cast<double>(firstLevel.accessCnt);
+        } else {
+            l1MissRate = 0.0;
+            avgTime    = 0.0;
+        }
+        if (secondLevel.accessCnt > 0) {
+            l2MissRate = static_cast<double>(secondLevel.missCnt)
+                         / static_cast<double>(secondLevel.accessCnt);
+        } else {
+            l2MissRate = 0.0;
+        }
+    }
+};
